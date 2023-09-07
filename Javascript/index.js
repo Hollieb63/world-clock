2008cffa27b375370732e44c4a289e145cac32ca
Hollieb63 <127986870+Hollieb63@users.noreply.github.com>
2023-09-07 17:04:33 +0100

    Created functionality for select

--- Javascript/index.js
+++ Javascript/index.js
@@ -24,3 +24,21 @@ let quebecTimeElement=quebecElement.querySelector(".time");
 quebecDateElement.innerHTML=moment().format("MMMM Do, YYYY");
 quebecTimeElement.innerHTML=moment().tz("Canada/Central").format("h:mm:ss [<small>]A[</small>]");
 },1000);
+
+function updateCity(event){
+    let cityTimeZone=event.target.value;
+    let cityName= cityTimeZone.split("/")[1];
+    let cityTime=moment().tz(cityTimeZone);
+    let citiesElement=document.querySelector("#cities");
+    citiesElement.innerHTML=
+    `<div class="city">
+    <div>
+    <h2>${cityName}</h2>
+    <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format("A")}</small></div>
+</div>`;
+}
+
+let selectCityElement=document.querySelector("#city");
+selectCityElement.addEventListener("change", updateCity);
