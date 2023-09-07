abb22b5dabec5e14680099f6dd7463b4f5c3e1ae
Hollieb63 <127986870+Hollieb63@users.noreply.github.com>
2023-09-07 16:22:25 +0100

    Added JS to the project

---
+++ Javascript/index.js
@@ -0,0 +1,26 @@
+setInterval(function(){
+
+let londonElement=document.querySelector("#london");
+let londonDateElement=londonElement.querySelector(".date");
+let londonTimeElement=londonElement.querySelector(".time");
+londonDateElement.innerHTML=moment().format("MMMM Do, YYYY");
+londonTimeElement.innerHTML=moment().tz("Europe/London").format("h:mm:ss [<small>]A[</small>]");
+
+let tokyoElement=document.querySelector("#tokyo");
+let tokyoDateElement=tokyoElement.querySelector(".date");
+let tokyoTimeElement=tokyoElement.querySelector(".time");
+tokyoDateElement.innerHTML=moment().format("MMMM Do, YYYY");
+tokyoTimeElement.innerHTML=moment().tz("Asia/Tokyo").format("h:mm:ss [<small>]A[</small>]");
+
+let seoulElement=document.querySelector("#seoul");
+let seoulDateElement=seoulElement.querySelector(".date");
+let seoulTimeElement=seoulElement.querySelector(".time");
+seoulDateElement.innerHTML=moment().format("MMMM Do, YYYY");
+seoulTimeElement.innerHTML=moment().tz("Asia/Seoul").format("h:mm:ss [<small>]A[</small>]");
+
+let quebecElement=document.querySelector("#quebec");
+let quebecDateElement=quebecElement.querySelector(".date");
+let quebecTimeElement=quebecElement.querySelector(".time");
+quebecDateElement.innerHTML=moment().format("MMMM Do, YYYY");
+quebecTimeElement.innerHTML=moment().tz("Canada/Central").format("h:mm:ss [<small>]A[</small>]");
+},1000);
